e12ff1be31fa2ef3f0f609ac8b2aedaf83958441
Aris Thallas <aris.thallas@gmail.com>
2016-05-19 16:34:31 +0300

    Fixed add new user script

--- utilities/dumpRAPPMysqlDatabase.sh
+++ utilities/dumpRAPPMysqlDatabase.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+##
+# Copyright 2015 RAPP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 i=0;
-for j in `cat /etc/db_credentials` 
+for j in `cat /etc/db_credentials`
 do
-   array[$i]=$j; 
+   array[$i]=$j;
    #do manipulations here
-    i=$(($i+1)); 
-   
-done 
+    i=$(($i+1));
+
+done
 echo "Value of third element in my array : ${array[1]} ";
 username=${array[0]};
 password=${array[1]};
 
-mysqldump -u $username -p$password RappStore > $HOME/mysqlBckup_$DATE
+mysqldump -u $username -p$password rapp_platform > $HOME/mysqlBckup_$DATE
--- utilities/importRAPPMysqlDatabase.sh
+++ utilities/importRAPPMysqlDatabase.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
+##
+# Copyright 2015 RAPP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 i=0;
-for j in `cat /etc/db_credentials` 
+for j in `cat /etc/db_credentials`
 do
-   array[$i]=$j; 
+   array[$i]=$j;
    #do manipulations here
-    i=$(($i+1)); 
-   
-done 
+    i=$(($i+1));
+
+done
 echo "Value of third element in my array : ${array[1]} ";
 username=${array[0]};
 password=${array[1]};
 
 echo "enter root mysql password"
-echo "drop database RappStore" | mysql -u root -p
+echo "drop database rapp_platform" | mysql -u root -p
 echo "enter root mysql password"
-echo "create database RappStore" | mysql -u root -p
+echo "create database rapp_platform" | mysql -u root -p
 echo "enter root mysql password"
-mysql -u root -p RappStore < $HOME/$1
+mysql -u root -p rapp_platform < $HOME/$1
